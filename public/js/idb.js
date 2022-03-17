97835a729471c747848659ad7d99faf863c715a8
Reht Cavazos <reht.dev@gmail.com>
2022-03-17 17:08:02 -0500

    minor changes

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,11 +5,11 @@ const request = indexedDB.open("budget-this", 1);
 
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore("new_item", { autoInrement: true });
+    db.createObjectStore("new_transaction", { autoInrement: true });
 };
 
 // on success 
-request.onsuccess = event => {
+request.onsuccess = function (event) {
     db = event.target.result;
     if (navigator.online) {
         // checkDB();
@@ -21,17 +21,26 @@ request.onerror = event => {
     console.log("Oops, something went wrong" + event.target.errorCode);
 };
 
+function offlineSave(record) {
+    // create transaction in db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    // access the object store
+    const store = transaction.objectStore("new_transaction");
+    // add the record to the store
+    store.add(record);
+};
+
 function checkDB() {
     // create transaction in db
-    const transaction = db.transaction(["new_item"], "readwrite");
+    const transaction = db.transaction(["new_transaction"], "readwrite");
     // access the object store
-    const store = transaction.objectStore("new_item");
+    const store = transaction.objectStore("new_transaction");
     // get all records from the store and assign variable
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-            fetch("/api/transaction", {
+            fetch("/api/transaction/bulk", {
                 method: "POST",
                 body: JSON.stringify(getAll.result),
                 headers: {
@@ -45,25 +54,16 @@ function checkDB() {
                         throw new Error(serverResponse);
                     }
                     // if successful, open a transaction on your pending db
-                    const transaction = db.transaction(["new_item"], "readwrite");
+                    const transaction = db.transaction(["new_transaction"], "readwrite");
                     // access your pending object store
-                    const store = transaction.objectStore("new_item");
-                    // clear all items in your store
+                    const store = transaction.objectStore("new_transaction");
+                    // clear all transactions in your store
                     store.clear();
 
-                    console.log("All items submitted");
+                    console.log("All transactions submitted");
                 });
         }
     };
-};
-
-function offlineSave(record) {
-    // create transaction in db
-    const transaction = db.transaction(['new_item'], 'readwrite');
-    // access the object store
-    const store = transaction.objectStore("new_item");
-    // add the record to the store
-    store.add(record);
 };
 
 // listen for online connection reestablish
